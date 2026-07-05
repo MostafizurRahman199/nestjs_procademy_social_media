39b550d72fe40625194b0b952decbf7d2b1cc52b
MostafizurRahman199 <fardilshifat199@gmail.com>
2026-07-05 22:51:04 +0600

    integrate ConfigModule and TypeORM with PostgreSQL support in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './users/users.module';
@@ -6,7 +8,25 @@ import { TweetModule } from './tweet/tweet.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, TweetModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('database_url'),
+        autoLoadEntities: true,
+        synchronize: true,
+        ssl: { rejectUnauthorized: false },
+      }),
+    }),
+    UserModule,
+    TweetModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
